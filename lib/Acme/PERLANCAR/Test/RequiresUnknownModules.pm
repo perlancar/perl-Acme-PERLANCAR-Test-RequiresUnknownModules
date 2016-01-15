fd4616fd3d18c98345448f6478bbc341544ae5aa
perlancar (@pc-home) <perlancar@gmail.com>
2016-01-15 21:26:05 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/RequiresUnknownModules.pm
@@ -0,0 +1,17 @@
+package Acme::PERLANCAR::Test::RequiresUnknownModules;
+
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Distribution that has prerequisites to modules not on CPAN
+
+=head1 DESCRIPTION
+
+This distribution is created to test tools like L<App::lcpan>. It contains
+prerequisites to modules that are not on CPAN.
